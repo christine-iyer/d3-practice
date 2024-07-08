a359d5cffd263b5a5dfdaca89ed4395c280eeb0d
chrisIyer <hellohotbox420@gmail.com>
2024-07-08 17:41:57 -0400

    first commit

---
+++ src/components/Todo.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+
+
+export default function Todo({ todo, completeTodo, editTodoText, deleteTodo }) {
+  const [showInput, setShowInput] = useState(false)
+  return (
+    <li style={{"listStyle": "none"}}>
+      <div className="left">
+        <h2
+          onClick={(e) => {
+            setShowInput(!showInput)
+          }}
+        >
+          {todo.text}
+        </h2>
+        <input
+          style={{ display: showInput ? "block" : "none" }}
+          type="text"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              editTodoText(todo.id, e)
+              setShowInput(false)
+            }
+          }}
+        />
+      </div>
+</li>
+
+  )
+}
